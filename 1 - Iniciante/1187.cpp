7b6cd47fdb07e68f2e5a42420ef59838704db8d4
Arthur Maximus <arthurmcb321@gmail.com>
2024-12-30 02:12:32 -0300

    1187 - Área Superior

---
+++ 1 - Iniciante/1187.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double matriz[12][12];
+    char c; cin >> c;
+
+    for (int i = 0; i < 12; i++){
+        for (int j = 0; j < 12; j++){
+            cin >> matriz[i][j];
+        }
+    }
+
+    double soma = 0, aux = 0;
+    int j = 0, k = 11, l = 1;
+    for (int i = 0; i < 5; i++){
+        for (j = l; j < k; j++){
+            soma += matriz[i][j];
+            aux++;
+        }
+        j++; k--; l++;
+    }
+
+    cout << fixed << setprecision(1);
+    switch(c){
+        case 'S':
+            cout << soma << "\n";
+            break;
+        case 'M':
+            cout << soma/aux << "\n";
+            break;
+    }
+}
