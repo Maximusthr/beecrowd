2041abf6d3ee2f5cfdab90b9b0c76f84a2516695
Arthur Maximus <arthurmcb321@gmail.com>
2025-01-08 01:50:22 -0300

    3302 - Resposta Certa

---
+++ 1 - Iniciante/3302.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int x; cin >> x;
+    
+    for (int i = 0; i < x; i++){
+        int aux; cin >> aux;
+        cout << "resposta " << i+1 << ": " << aux << "\n";
+    }
+}
