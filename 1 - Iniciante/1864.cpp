ba49a0fcfa7f38742ec2a97cb26aff9d9819ec7a
Arthur Maximus <arthurmcb321@gmail.com>
2025-01-12 02:33:13 -0300

    1864 - Nossos Dias Nunca Voltarão

---
+++ 1 - Iniciante/1864.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string frase = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+
+    int x; cin >> x;
+
+    for (int i = 0; i < x; i++){
+        cout << frase[i];
+    }
+
+    cout << "\n";
+}
