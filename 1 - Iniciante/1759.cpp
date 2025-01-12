253ad82d811b7117884f218be0fa4feb06f85d3d
Arthur Maximus <arthurmcb321@gmail.com>
2025-01-12 01:44:47 -0300

    1759 - Ho Ho Ho

---
+++ 1 - Iniciante/1759.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int x; cin >> x;
+
+    for (int i = 0; i < x; i++){
+        if (i == x-1) cout << "Ho!\n";
+        else cout << "Ho ";
+    }
+}
