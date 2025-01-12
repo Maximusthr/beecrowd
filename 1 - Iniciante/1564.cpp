e2f829e9e01de977e662fabbecafd4c1f441ba34
Arthur Maximus <arthurmcb321@gmail.com>
2025-01-12 01:40:06 -0300

    1564 - Vai Ter Copa?

---
+++ 1 - Iniciante/1564.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int x;
+
+    int soma = 0;
+    while(cin >> x){
+        if (x == 0) cout << "vai ter copa!\n";
+        else cout << "vai ter duas!\n";
+    }
+}
