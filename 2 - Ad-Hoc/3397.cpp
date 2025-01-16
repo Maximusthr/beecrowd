76af5fc11e6da9f4df31c3384e18dc264293b3f1
Arthur Maximus <arthurmcb321@gmail.com>
2025-01-15 23:05:35 -0300

    3397 - Melhor do Multiverso

---
+++ 2 - Ad-Hoc/3397.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    string x;
+
+    while(getline(cin, x)){
+        cout << "eu" << "\n";
+    }
+}
