d0a686bfbb403a50d6c851c47dcf463cb49281ad
Arthur Maximus <arthurmcb321@gmail.com>
2025-01-21 00:00:00 -0300

    1924 - Vitória e a Indecisão

---
+++ 1 - Iniciante/1924.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int x; cin >> x;
+
+    for (int i = 0; i < x; i++){
+        string aux; getline(cin, aux);
+    }
+
+    cout << "Ciencia da Computacao\n";
+}
