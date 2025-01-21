77378d92014b9a7ee7d675321e7ce1ebe270f1e9
Arthur Maximus <arthurmcb321@gmail.com>
2025-01-21 15:24:50 -0300

    1984 - O Enigma do Pronalândia

---
+++ 1 - Iniciante/1984.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string x; cin >> x;
+
+    for (int i = (int)x.size()-1; i >= 0; i--){
+        cout << x[i];
+    }
+
+    cout << "\n";
+}
